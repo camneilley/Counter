27a836390660c8c7cf89c92cb258e17a6d3c1b69
Cameron Neilley <crn52@tux.cs.drexel.edu>
2014-04-17 20:45:42 -0400

    GUI setup

---
+++ src/Counter.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Counter {
+	public static void main(String[] args){
+		CounterFrame lp=new CounterFrame();
+		lp.setSize(1000,500);
+		lp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		lp.setVisible(true);
+	}
+}
--- src/CounterFrame.java
+++ src/CounterFrame.java
@@ -1,20 +1,18 @@
 import javax.print.attribute.standard.Media;
 import javax.swing.*;
-
 import java.awt.*;
 import java.awt.event.*;
 import java.util.ArrayList;
 
-import javafx.scene.media.MediaPlayer;
 public class CounterFrame extends JFrame{
+	//Make empty array for pictures
 	protected static ArrayList<ImageIcon> tiles=new ArrayList<ImageIcon>();
+	//initialize all pictures
 	ImageIcon marik1=new ImageIcon("images/MarikTileLeft.jpg");
 	ImageIcon marik2=new ImageIcon("images/MarikTileRight.jpg");
 	ImageIcon yugi1=new ImageIcon("images/YugiTileLeft");
 	ImageIcon yugi2=new ImageIcon("images/YugiTileRight");
-	
-	MediaPlayer player=new MediaPlayer(new Media("music/song1"));
-	
+	//initialize all GUI components, 1:left;2:right
 	JButton picture1;
 	JButton picture2;
 	
@@ -51,22 +49,29 @@ public class CounterFrame extends JFrame{
 	JButton soundOn;
 	JButton soundOff;
 	JButton reset;
-	
+	//Declare Boxes for organization
 	Box left;
+	Box incrowa1;
+	Box incrowb1;
+	Box incrowc1;
 	Box right;
+	Box incrowa2;
+	Box incrowb2;
+	Box incrowc2;
 	Box bottom;
-	
+	//Add pictures to array of pictures
 	public void addPlayerTiles(){
 		tiles.add(marik1);
 		tiles.add(marik2);
 		tiles.add(yugi1);
 		tiles.add(yugi2);
 	}
-	public void addMusic(){
-		musics.add()
-	}
+	//begin constructor
 	public CounterFrame(){
-		picture1=new JButton(tiles.get(3));
+		addPlayerTiles();
+		setLayout(new FlowLayout());
+		//initialize GUI components
+		picture1=new JButton(tiles.get(1));
 		picture2=new JButton(tiles.get(1));
 		
 		name1=new JTextArea("Player1");
@@ -106,6 +111,69 @@ public class CounterFrame extends JFrame{
 		plusc2=new JButton(new ImageIcon("images/plus.jpg"));
 		
 		music=new JButton(new ImageIcon("images/music.jpg"));
+		String[] songs={"Song1","Song2","Song3"};
+		musicMenu=new JComboBox(songs);
+		leaderboard=new JButton("Leaderboards");
+		soundOn=new JButton(new ImageIcon("images/music.jpg"));
+		soundOff=new JButton(new ImageIcon("images/music.jpg"));
+		reset=new JButton("Reset");
+		//initialize containers
+		left=Box.createVerticalBox();
+		incrowa1=Box.createHorizontalBox();
+		incrowb1=Box.createHorizontalBox();
+		incrowc1=Box.createHorizontalBox();
+		
+		right=Box.createVerticalBox();
+		incrowa2=Box.createHorizontalBox();
+		incrowb2=Box.createHorizontalBox();
+		incrowc2=Box.createHorizontalBox();
+		
+		bottom=Box.createHorizontalBox();
+		//add components to left box, incrementors in separate boxes
+		left.add(picture1);
+		left.add(name1);
+		left.add(lifePoints1);
+			incrowa1.add(minusa1);
+			incrowa1.add(inca1);
+			incrowa1.add(plusa1);
+		left.add(incrowa1);
+			incrowb1.add(minusb1);
+			incrowb1.add(incb1);
+			incrowb1.add(plusb1);
+		left.add(incrowb1);
+			incrowc1.add(minusc1);
+			incrowc1.add(incc1);
+			incrowc1.add(plusc1);
+		left.add(incrowc1);
+		//Add to right box
+		right.add(picture2);
+		right.add(name2);
+		right.add(lifePoints2);
+			incrowa2.add(minusa2);
+			incrowa2.add(inca2);
+			incrowa2.add(plusa2);
+		right.add(incrowa2);
+			incrowb2.add(minusb2);
+			incrowb2.add(incb2);
+			incrowb2.add(plusb2);
+		right.add(incrowb2);
+			incrowc2.add(minusc2);
+			incrowc2.add(incc2);
+			incrowc2.add(plusc2);
+		right.add(incrowc2);
+		//Add to bottom box
+		bottom.add(music);
+		bottom.add(musicMenu);
+		bottom.add(leaderboard);
+		bottom.add(reset);
+		bottom.add(soundOn);
+		bottom.add(soundOff);
+		//Add boxes to main container
+		add(left);
+		add(right);
+		add(bottom);
+		
+		
 		
 		
 	}
